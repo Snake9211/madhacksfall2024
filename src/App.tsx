9cf024ec49e2830b9dadbd1c5ec113cf6de2b80f
Nikhil Tiwari <tiwarin9211@gmail.com>
2024-11-09 15:55:43 -0600

    Added navigation bar with links to other pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
 import './App.css'
 
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
 import Compare from './pages/Resources'
+import Search from './pages/Search';
 
-function App() {
+// import Submission from './pages/Submission';
+// import Trends from './pages/Trends';
 
+function NavigationBar() {
   return (
-    <>
-      <div>
-        <Compare />
-      </div>
-    </>
-  )
+    <nav>
+      <a href="/">Search </a>
+      <a href="/submission">Submission </a>
+      <a href="/trends">Trends </a>
+      <a href="/resources">Resources </a>
+      
+    </nav>
+  );
 }
 
-export default App
+function MainApp() {
+  return (
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Search />} />
+        {/* <Route path="/submission" element={<Submission />} />
+        <Route path="/trends" element={<Trends />} /> */}
+        <Route path="/resources" element={<Compare />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default MainApp;
