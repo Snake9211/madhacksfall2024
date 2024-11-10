05dfe1e11824dc4056988504680d1c2f3479b55f
Nikhil Tiwari <tiwarin9211@gmail.com>
2024-11-09 21:22:05 -0600

    Changes the navBar again.

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,12 +1,11 @@
 import { Disclosure, DisclosureButton } from '@headlessui/react';
-
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const navigation = [
-  { name: 'Search', href: '/', current: true },
-  { name: 'Submission', href: '/submission', current: false },
-  { name: 'Trends', href: '/trends', current: false },
-  { name: 'Resources', href: '/resources', current: false },
+  { name: 'Search', href: '/' },
+  { name: 'Submission', href: '/submission' },
+  { name: 'Trends', href: '/trends' },
+  { name: 'Resources', href: '/resources' },
 ];
 
 function classNames(...classes: string[]) {
@@ -14,10 +13,21 @@ function classNames(...classes: string[]) {
 }
 
 export default function NavBar() {
+  const location = useLocation();
+
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" style={{ backgroundColor: '#c5050c' }}>
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
+          
+          <div className="flex-shrink-0">
+            <img
+              src="../badger_shield.png" 
+              alt="Logo"
+              className="h-20 w-20" 
+            />
+          </div>
+
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
@@ -26,7 +36,9 @@ export default function NavBar() {
                     key={item.name}
                     to={item.href}
                     className={classNames(
-                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      location.pathname === item.href
+                        ? 'bg-white text-red-800' // White background with red text for active link
+                        : 'text-white hover:bg-red-700 hover:text-white', // Hover effect with darker red
                       'rounded-md px-3 py-2 text-sm font-medium'
                     )}
                   >
@@ -47,7 +59,9 @@ export default function NavBar() {
               as={Link}
               to={item.href}
               className={classNames(
-                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                location.pathname === item.href
+                  ? 'bg-white text-red-800' // White background with red text for active link
+                  : 'text-white hover:bg-red-700 hover:text-white', // Hover effect with darker red
                 'block rounded-md px-3 py-2 text-base font-medium'
               )}
             >
